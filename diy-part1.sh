bb9cd23b791ca9bd62faaa6bbfb62fa3954c659a
zzlyung <124063972+zzlyung@users.noreply.github.com>
2025-01-12 21:33:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,5 @@ echo 'src-git small https://github.com/kenzok8/small' >> ./feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
+echo "src-git turboacc https://github.com/chenmozhijin/turboacc.git" >> ./feeds.conf.default
+echo "src-git ssrp https://github.com/fw876/helloworld.git" >> ./feeds.conf.default
