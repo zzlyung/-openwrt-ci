a7dd30b2cd9523f2f3ccc3bd544b1984582052df
zzlyung <124063972+zzlyung@users.noreply.github.com>
2025-01-12 21:31:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,9 +12,9 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-echo 'src-git NueXini_Packages https://github.com/zzlyung/NueXini_Packages.git' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> ./feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> ./feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> ./feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> ./feeds.conf.default
+
